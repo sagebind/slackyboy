03f7ae6fd760dbcacb919d33e5881f9c6ecf00a1
Pavel Voronin <pavel.a.voronin@gmail.com>
2015-10-07 17:20:46 +0300

    replace Human plugin with Greetings plugin

---
+++ src/Plugins/Greetings/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+namespace Slackyboy\Plugins\Greetings;
+
+use Slack\User;
+use Slackyboy\Message;
+use Slackyboy\Plugins\AbstractPlugin;
+
+/**
+ * Class Plugin
+ */
+class Plugin extends AbstractPlugin
+{
+    public $greeting = 'Greetings';
+
+    public function enable()
+    {
+        $this->bot->on('mention', function (Message $message) {
+            if ($message->matchesAny('/hello/i', '/greetings/i')) {
+                $message->getChannel()->then(function ($channel) use ($message) {
+                    $message->getUser()->then(function (User $user) use ($channel) {
+                        $greetings = sprintf('%s, %s!', $this->greeting, $user->getUsername());
+                        $this->bot->say($greetings, $channel);
+                    });
+                });
+            }
+        });
+    }
+}
