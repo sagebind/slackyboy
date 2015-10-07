ddfe593a9ea90d6b324b4711f7c0329c22a95562
Pavel Voronin <pavel.a.voronin@gmail.com>
2015-10-07 13:06:16 +0300

    initialize plugins with options from configuration

--- src/Plugins/PluginManager.php
+++ src/Plugins/PluginManager.php
@@ -41,27 +41,38 @@ class PluginManager
     /**
      * Dynamically loads a plugin by name.
      *
-     * @param string $className
+     * @param string $className Fully qualified plugin class name
+     * @param array  $options   Options
+     *
      * @throws \Exception
      */
-    public function load($className)
+    public function load($className, $options)
     {
         if (!class_exists($className)) {
-            throw new \Exception('The plugin class "'.$className.'" could not be found.');
+            throw new \Exception('The plugin class "' . $className . '" could not be found.');
         }
 
         // make sure the class implements the plugin interface
         if (!in_array(PluginInterface::class, class_implements($className))) {
-            throw new \Exception('The class "'.$className.'" is not a plugin.');
+            throw new \Exception('The class "' . $className . '" is not a plugin.');
         }
 
         try {
             /** @var PluginInterface $instance */
             $instance = new $className($this->bot, $this);
+
+            foreach ($options as $key => $value) {
+                if (property_exists($instance, $key)) {
+                    $instance->$key = $value;
+                } else {
+                    throw new \Exception('Unknown plugin parameter: ' . $key);
+                }
+            }
+
             $instance->enable();
             $this->plugins[$className] = $instance;
         } catch (\Exception $exception) {
-            throw new \Exception('Error in loading plugin "'.$className.'"');
+            throw new \Exception('Error in loading plugin "' . $className . '": ' . $exception->getMessage());
         }
     }
 
